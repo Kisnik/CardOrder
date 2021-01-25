e209ce8d8f79a26c2e8480f18348319f01692ab6
Nikolai Kiselev <kisnikn@mail.ru>
2021-01-25 21:39:25 +0300

    CardOrderTest edited

--- src/test/java/ru/netology/web/CardOrderTest.java
+++ src/test/java/ru/netology/web/CardOrderTest.java
@@ -1,6 +1,8 @@
 package ru.netology.web;
 
 import com.codeborne.selenide.SelenideElement;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 
@@ -9,32 +11,41 @@ import static com.codeborne.selenide.Selenide.*;
 
 class CardOrderTest {
 
+    @BeforeEach
+    void openWebSite() {
+        open("http://localhost:7777");
+    }
+
 //успешная отправка
     @Test
     void shouldSuccess() {
-        open("http://localhost:7777");
+
         $("[type=text]").setValue("Василий Васильев");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $(".paragraph").shouldHave(exactText("  Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
+        $("[data-test-id=order-success]")
+                .shouldHave(exactText("  Ваша заявка успешно отправлена! " +
+                        "Наш менеджер свяжется с вами в ближайшее время."));
     }
    //ФИО по-английски
     @Test
     void shouldHaveEnglishSymbols() {
-        open("http://localhost:7777");
+
         $("[type=text]").setValue("Vasily");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $(".input_type_text > .input__inner > .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $(".input_type_text > .input__inner > .input__sub")
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. " +
+                        "Допустимы только русские буквы, пробелы и дефисы."));
 
     }
 
     //ФИО с цифрами
     @Test
     void shouldHaveNumbersFIO() {
-        open("http://localhost:7777");
+
         $("[type=text]").setValue("В1силий");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
@@ -46,71 +57,102 @@ class CardOrderTest {
     //ФИО со спецсимволами
     @Test
     void shouldHaveSpecialCharacters() {
-        open("http://localhost:7777");
+
         $("[type=text]").setValue("В@силий");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $(".input_type_text > .input__inner > .input__sub").shouldHave(exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
+        $(".input_type_text > .input__inner > .input__sub")
+                .shouldHave(exactText("Имя и Фамилия указаные неверно. " +
+                        "Допустимы только русские буквы, пробелы и дефисы."));
 
     }
 
     //номер телефона без + в начале
     @Test
     void shouldHaveNotPlus() {
-        open("http://localhost:7777");
+
         $("[type=text]").setValue("Василий Васильев");
         $("[data-test-id=phone] input").setValue("79270000000");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $(".input_type_tel > .input__inner > .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(".input_type_tel > .input__inner > .input__sub")
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
 
     }
 
     //номер телефона имеет меньше 11 цифр
     @Test
     void shouldHaveLessThanElevenNumbers() {
-        open("http://localhost:7777");
+
         $("[type=text]").setValue("Василий Васильев");
         $("[data-test-id=phone] input").setValue("+7927000000");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $(".input_type_tel > .input__inner > .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(".input_type_tel > .input__inner > .input__sub")
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
 
     }
 
     //номер телефона имеет больше 11 цифр
     @Test
     void shouldHaveMoreThanElevenNumbers() {
-        open("http://localhost:7777");
+
         $("[type=text]").setValue("Василий Васильев");
         $("[data-test-id=phone] input").setValue("+792700000000");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $(".input_type_tel > .input__inner > .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(".input_type_tel > .input__inner > .input__sub")
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
 
     }
 
     //номер телефона имеет буквы
     @Test
     void shouldNumberHaveCharacters() {
-        open("http://localhost:7777");
+
         $("[type=text]").setValue("Василий Васильев");
         $("[data-test-id=phone] input").setValue("+7927000000f");
         $("[data-test-id=agreement]").click();
         $("[type=button]").click();
-        $(".input_type_tel > .input__inner > .input__sub").shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(".input_type_tel > .input__inner > .input__sub")
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
 
     }
 
     //нет согласия по персональным данным
     @Test
     void shouldAgreementFalse() {
-        open("http://localhost:7777");
+
         $("[type=text]").setValue("Василий Васильев");
         $("[data-test-id=phone] input").setValue("+79270000000");
         $("[type=button]").click();
-        $(".input_invalid").shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+        $(".input_invalid")
+                .shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих " +
+                        "персональных данных и разрешаю сделать запрос в бюро кредитных историй"));
+    }
+
+    //ФИО не заполнено
+    @Test
+    void shouldFIOEmpty() {
+
+        $("[data-test-id=phone] input").setValue("+79270000000");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_text > .input__inner > .input__sub")
+                .shouldHave(exactText("Поле обязательно для заполнения"));
+    }
+
+    //номер телефона не заполнен
+    @Test
+    void shouldPhoneEmpty() {
+
+        $("[type=text]").setValue("Василий Васильев");
+        $("[data-test-id=agreement]").click();
+        $("[type=button]").click();
+        $(".input_type_tel > .input__inner > .input__sub")
+                .shouldHave(exactText("Поле обязательно для заполнения"));
+
     }
 }
 
